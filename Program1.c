aec022134f186a5dc50507090236387e57e09a03
Rajesh Kumar Padhy <i.am.the.rajesh@gmail.com>
2022-07-25 18:08:58 +0530

    Add Author

--- Program1.c
+++ Program1.c
@@ -1,6 +1,7 @@
 /**
  * @file Program1.c
  * @author Sandhyarani Jena (sandhya.rani.jena@outlook.com)
+ * @author Rajesh Kumar Padhy (i.am.the.rajesh@gmail.com)
  * @brief My Program
  * @version 0.1
  * @date 2022-07-25
